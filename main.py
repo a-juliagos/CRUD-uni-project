522672ba223c932f7ff4b32f62524691938fd1f8
juoli9 <juliagabsjgtcos@gmail.com>
2026-06-01 11:49:13 -0300

    Alteracoes semifinais antes das novas branchs

--- main.py
+++ main.py
@@ -27,23 +27,12 @@ class TratamentoStatus(IntEnum):
 ativos = []
 id_ativo = 1
 
-def menu_cadastro():
-    
-    print("""Opções de cadastro: 
-               
-   1 - Cadastrar Ativo
-   2 - Cadastrar Vulnerabilidade             
-     """)
-    
-    escolha = int(input('Escolha uma opção para continuar: '))
+def limpar_tela():
 
-    match escolha:
+    os.system('cls')
 
-        case 1: 
-            cadastrar_ativo()
-
-        case 2:
-            cadastrar_vuln()
+def pausar():
+    input("\nPressione ENTER para continuar...")
 
 
 def cadastrar_ativo():
@@ -54,9 +43,9 @@ def cadastrar_ativo():
 
     while True:
 
-        nome_host = input('Nome ou hostname: ')
+        nome_host = input('Nome ou hostname: ').strip()
 
-        if any(nome_host == ativo["nome_hostname"] for ativo in ativos):
+        if any(nome_host.lower() == ativo["nome_hostname"].lower() for ativo in ativos):
             print('Este nome já está em uso!\n')
         
         else:
@@ -85,71 +74,73 @@ def cadastrar_ativo():
     print('Ativo cadastrado com sucesso!!')
     id_ativo += 1 
         
-    sleep(3)
-    return
+    pausar()
 
 
 def cadastrar_vuln():
         
-        ativo = conferir_ativo()
+    ativo = buscar_ativo()
 
-        if ativo:
+    if ativo:
             
-            descricao = input('Descrição: ')
-            tipo = input('Tipo: ')
+        descricao = input('Descrição: ')
+        tipo = input('Tipo: ')
 
-            print('\n---- Severidade ----\n')
+        print('\n---- Severidade ----\n')
 
-            for sev in SeveridadeTipo:
-                print(f'   {sev.value} - {sev.name}')
+        for sev in SeveridadeTipo:
+            print(f'   {sev.value} - {sev.name}')
     
-            sev_tipo = SeveridadeTipo(int(input('Severidade: ')))
+        sev_tipo = SeveridadeTipo(int(input('Severidade: ')))
 
-            print('\n---- Status de Tratamento ----\n')
+        print('\n---- Status de Tratamento ----\n')
 
-            for status in TratamentoStatus:
+        for status in TratamentoStatus:
                 print(f'   {status.value} - {status.name}')
     
-            status_tipo = TratamentoStatus(int(input('Status de Tratamento: ')))
+        status_tipo = TratamentoStatus(int(input('Status de Tratamento: ')))
 
 
-            vulnerabilidades = {
+        vulnerabilidades = {
         "descricao" : descricao,
         "tipo" : tipo,
         "severidade" : sev_tipo.name,
         "status" : status_tipo.name
         }
 
-            ativo["vulnerabilidades"].append(vulnerabilidades)
+        ativo["vulnerabilidades"].append(vulnerabilidades)
 
+        print("Vulnerabilidade cadastrada com sucesso!")
 
-def conferir_ativo():
-
-    if len(ativos) == 0 :
-        print('Não existem Ativos cadastrados!!')
-        return None
+    pausar()
     
-    elif len(ativos) > 0:
-        
-        ativo_buscado = input('Digite o nome/hostname ou ID do ativo buscado: ')
-        ativo = buscar_ativo(ativo_buscado)
 
-        if ativo:
-            return ativo
+def buscar_ativo():
 
-    return None
+    if not ativos:
+        print('Não existem Ativos cadastrados!!')
+        pausar()
+        return None
 
-
-def buscar_ativo(ativo_buscado):
+    ativo_buscado = input(
+        'Digite o nome/hostname ou ID do ativo buscado: '
+    )
 
     if ativo_buscado.isdigit():
         ativo_buscado = int(ativo_buscado)
 
     for ativo in ativos:
-        if ativo_buscado == ativo["ID"] or ativo_buscado == ativo["nome_hostname"]:
-
-            return ativo
         
+        if (
+        isinstance(ativo_buscado, str)
+        and ativo_buscado.lower() == ativo["nome_hostname"].lower() ): 
+            return ativo
+
+        if ativo_buscado == ativo["ID"]:
+            return ativo
+    
+    print('O ativo não existe ou foi excluído.')
+    pausar()
     return None
 
 
@@ -166,57 +157,75 @@ Tipo: {ativo["tipo"]}
 
 ---- Vulnerabilidades --- """)
     
-    if len(ativo["vulnerabilidades"]) == 0:
+    if not ativo["vulnerabilidades"]:
 
         print('\nNão existem Vulnerabilidades cadastradas!!')
 
-    elif len(ativo["vulnerabilidades"]) > 0:
+    else:
 
-        for vuln in ativo["vulnerabilidades"]:
+        for i, vuln in enumerate(ativo["vulnerabilidades"], start=1):
             
-            print(f"""                             
+            print(f"""   
+Vulnerabilidade {i}
+
 Descrição: {vuln["descricao"]}
 Tipo: {vuln["tipo"]}
 Severidade: {vuln["severidade"]}
-Status: {vuln["status"]}   """)
+Status: {vuln["status"]} """)
+            
+
+def listar_all_ativos():
+
+    if not ativos:
+        print("Não existem ativos cadastrados.")
+        pausar()
+        return
+
+    for ativo in ativos:
+        listar_ativo(ativo)
+
+    pausar()
 
 
 def atualizar_ativo():
 
-    ativo = conferir_ativo()
-
-    if ativo:
-        
-        while True:
-            
-            novo_nome_host = input('Digite o novo nome: ')
-            
-            if any( novo_nome_host == a["nome_hostname"] for a in ativos if a["ID"] != ativo["ID"] ):
-                print('Este nome já está em uso!')
-
-            else:
-                break
-
-        novo_responsavel = input('Digite o novo responsável: ')
-        novo_setor = input('Digite o novo setor: ')
-
-        ativo["nome_hostname"] = novo_nome_host
-        ativo["responsavel"] = novo_responsavel
-        ativo["setor"] = novo_setor
-
-        print('Ativo atualizado com sucesso!!')
+    ativo = buscar_ativo()
 
     if not ativo:
+        
         print('O ativo não existe ou foi excluído.')
+        pausar()
         return
+    
+    while True:
+            
+        novo_nome_host = input('Digite o novo nome: ')
+            
+        if any( novo_nome_host == a["nome_hostname"] for a in ativos if a["ID"] != ativo["ID"] ):
+            print('Este nome já está em uso!')
 
+        else:
+            break
+
+    novo_responsavel = input('Digite o novo responsável: ')
+    novo_setor = input('Digite o novo setor: ')
+
+    ativo["nome_hostname"] = novo_nome_host
+    ativo["responsavel"] = novo_responsavel
+    ativo["setor"] = novo_setor
+
+    print('Ativo atualizado com sucesso!!')
+
+    pausar()
+   
 
 def excluir_ativo():
 
-    ativo = conferir_ativo()
-    nome = ativo["nome_hostname"]
-
+    ativo = buscar_ativo()
+    
     if ativo:
+
+        nome = ativo["nome_hostname"]
 
         ativos.remove(ativo)
         print(f'O ativo {nome} foi excluído com sucesso!!')
@@ -224,14 +233,18 @@ def excluir_ativo():
     else:
         print('O ativo não existe ou foi excluído.')
 
+    pausar()
+   
 
 while True:
+
+    limpar_tela()
 
     print("""
 ---- Bem Vindo ao Sistema de Cadastro ----
    
    1 - Cadastrar Ativo/Vulnerabilidade
-   2 - Buscar
+   2 - Buscar/Listar
    3 - Atualizar
    4 - Remover
    0 - Sair 
@@ -244,25 +257,61 @@ while True:
         case 0:
 
             print('Encerrando sistema...')
-            sleep(3)
+            sleep(2)
             break
         
         case 1:
 
-            menu_cadastro()
+            limpar_tela()
+            
+            print("""Opções de cadastro: 
+               
+   1 - Cadastrar Ativo
+   2 - Cadastrar Vulnerabilidade             
+     """)
+    
+            escolha = int(input('Escolha uma opção para continuar: '))
+
+            match escolha:
+                
+                case 1: 
+                    cadastrar_ativo()
+
+                case 2:
+                    cadastrar_vuln()
+                
+                case _:
+                    print("Escolha um valor válido.")
 
         case 2:
 
-           certo = conferir_ativo()
+            limpar_tela()
+            
+            print("""Opções de busca: 
+               
+   1 - Buscar 
+   2 - Listar todos            
+     """)
+    
+            escolha = int(input('Escolha uma opção para continuar: '))
 
-           if certo :
-               
-               listar_ativo(certo)
+            match escolha:
+                
+                case 1: 
+                    ativo_certo = buscar_ativo()
+                    
+                    if ativo_certo :
+                        limpar_tela()
+                        listar_ativo(ativo_certo)
+                        pausar()
+                                     
+                case 2:
+                    limpar_tela()
+                    listar_all_ativos()
 
-           else:
-               
-               print('O ativo não existe ou foi excluído.')
-               
+                case _:
+                    print("Escolha um valor válido.")
+     
         case 3:
 
             atualizar_ativo()
@@ -270,3 +319,7 @@ while True:
         case 4:
 
             excluir_ativo()
+        
+        case _:
+            print("Escolha um valor válido.")
+            pausar()
